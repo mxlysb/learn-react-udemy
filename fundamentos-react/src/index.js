f96d89cff0344b849713121afe68cf0250a36e20
maely brandão <56039385+mxlysb@users.noreply.github.com>
2022-02-02 18:45:06 -0300

    criando arquivo css

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const tag = <strong>Olá React!</strong>
+
+ReactDOM.render(
+    <div>
+        { tag }
+    </div>,
+    document.getElementById('root')
+)
