497d57a9198fef6c40d90266a41883164a28502f
maely brandão <56039385+mxlysb@users.noreply.github.com>
2022-02-02 19:42:36 -0300

    criando componente simples

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,11 +2,12 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-const tag = <strong>Olá React!</strong>
+// Primeiro não precisa se chamar 'Primeiro'
+import Primeiro from './components/basicos/Primeiro'
 
 ReactDOM.render(
     <div>
-        { tag }
+        <Primeiro></Primeiro>
     </div>,
     document.getElementById('root')
 )
