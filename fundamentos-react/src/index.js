958cb00d7d2c07b9f91faea86b5ba32001941abe
maely brandão <56039385+mxlysb@users.noreply.github.com>
2022-02-03 11:30:13 -0300

    componente com parametros

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -4,10 +4,15 @@ import React from 'react'
 
 // Primeiro não precisa se chamar 'Primeiro'
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <Primeiro></Primeiro>
+        <ComParametro
+            titulo="Situação do Aluno"
+            aluno="Maria"
+            nota= { 9.3 } />
     </div>,
     document.getElementById('root')
 )
