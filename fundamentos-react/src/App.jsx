18f03c584c2d44d0f354c01d033c4edae9fea8d0
maely brandão <56039385+mxlysb@users.noreply.github.com>
2022-02-03 15:31:41 -0300

    cores nos cards

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -16,22 +16,22 @@ export default _ =>
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
-            <Card titulo="#04 - Desafio Aleatório">
+            <Card titulo="#04 - Desafio Aleatório" color="#581845">
                 <Aleatorio min={1} max={60} />
             </Card>
 
-            <Card titulo="#03 - Fragmento">
+            <Card titulo="#03 - Fragmento" color="#900C3F">
                 <Fragmento />
             </Card>
         
-            <Card titulo="#02 - Com Parâmetro">
+            <Card titulo="#02 - Com Parâmetro" color="#C70039">
                 <ComParametro
                     titulo="Situação do Aluno"
                     aluno="Maria"
                     nota= {9.3} />
             </Card>
 
-            <Card titulo="#01 - Primeiro Componente">
+            <Card titulo="#01 - Primeiro Componente" color="#FF5733">
                 <Primeiro></Primeiro>
             </Card>
         </div>
