5101c87b912ef4bf7fa5e71a323e6f2ee0090efb
maely brandão <56039385+mxlysb@users.noreply.github.com>
2022-02-04 16:01:08 -0300

    componente controlado

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -15,6 +15,7 @@ import ParOuImpar from "./components/condicional/ParOuImpar"
 import UsuarioInfo from "./components/condicional/UsuarioInfo"
 import DiretaPai from "./components/comunicacao/DiretaPai"
 import IndiretaPai from "./components/comunicacao/IndiretaPai"
+import Input from "./components/formulario/Input"
 
 //não precisa nomear usando default
 //(props); (); _ apenas um parametro
@@ -24,6 +25,10 @@ export default _ =>
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+            <Card titulo="#11 - Componente Controlado" color="#7D3C98">
+                <Input></Input>
+            </Card>
+
             <Card titulo="#10 - Comunicação Indireta" color="#B9770E">
                 <IndiretaPai></IndiretaPai>
             </Card>
