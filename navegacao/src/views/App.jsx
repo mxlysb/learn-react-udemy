67406a57219ddcdbf3db5796013a9e47affc1bf7
maely brandão <56039385+mxlysb@users.noreply.github.com>
2022-02-09 16:30:30 -0300

    organizando usando router

--- navegacao/src/views/App.jsx
+++ navegacao/src/views/App.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 import Content from "../components/layout/Content"
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import Menu from "../components/layout/Menu"
 import './App.css'
 
 const App = props => (
     <div className="App">
-        <Menu />
-        <Content />
+        <Router>
+            <Menu />
+            <Content />
+        </Router>
     </div>
 )
 
