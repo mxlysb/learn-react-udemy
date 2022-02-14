dfa4f46e70dc3c82a2b3226964a9986e9818501d
maely brandão <56039385+mxlysb@users.noreply.github.com>
2022-02-14 10:36:31 -0300

    desafio useEffect

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 function calcFatorial(num) {
     const n = parseInt(num)
@@ -23,12 +24,20 @@ const UseEffect = (props) => {
         }
     }, [fatorial])
 
+    // Ex #02
+    const [status, setStatus] = useState("Ímpar")
+
+    useEffect(function() {
+        setStatus(number % 2 === 0 ? "Par" : "Ímpar")
+    }, [number])
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <SectionTitle title="Exercício #01 "/>
             <div className="center">
                 <div>
                     <span className='text'>Fatorial: </span>
@@ -38,6 +47,14 @@ const UseEffect = (props) => {
                 <input type="number" className='input' 
                     value={number}
                     onChange={e => setNumber(e.target.value)} />
+            </div>
+
+            <SectionTitle title="Exercício #02 "/>
+            <div className="center">
+                <div>
+                    <span className='text'>Status: </span>
+                    <span className='text red'>{status}</span>
+                </div>
             </div>
         </div>
     )
