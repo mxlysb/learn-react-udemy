de89110f03bb13591558533d8a2abb54054de6a8
maely brandão <56039385+mxlysb@users.noreply.github.com>
2022-02-21 13:35:11 -0300

    criando um hook

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,31 +1,8 @@
 import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
-const initialState = {
-    cart: [],
-    products: [],
-    user: null,
-    number: 0
-}
-
-function reducer(state, action) {
-    switch(action.type) {
-        case 'numberAdd2':
-            return {...state, number: state.number + 2}
-        case 'login':
-            return {...state, user: { name: action.payload }}
-        case 'vezes7':
-                return {...state, number: state.number * 7}
-        case 'divide25':
-                return {...state, number: state.number / 25}
-        case 'numberAddN':
-                return {...state, number: state.number + action.payload}
-        case 'numberParseInt':
-            return {...state, number: parseInt(state.number)}
-        default:
-            return state
-    }
-}
+import { initialState, reducer } from '../../store'
+import { numberAdd2, login} from '../../store/actions'
 
 const UseReducer = (props) => {
     const [state, dispatch] = useReducer(reducer, initialState)
@@ -44,9 +21,13 @@ const UseReducer = (props) => {
                 <span className="text">{state.number}</span>
                 <div>
                     <button className="btn"
-                        onClick={() => dispatch({type: 'login', payload: 'Maria'})}>Login</button>
+                        onClick={() => login(dispatch, 'João')}>
+                            Login
+                    </button>
                     <button className="btn"
-                        onClick={() => dispatch({type: 'numberAdd2'})}>+2</button>
+                        onClick={() => numberAdd2(dispatch)}>
+                            +2
+                    </button>
                     <button className="btn"
                         onClick={() => dispatch({type: 'vezes7'})}>x7</button>
                     <button className="btn"
